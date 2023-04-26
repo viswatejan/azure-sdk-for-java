55a79855ed6dcd67c205c4c544fc3ae80f0b3216
José Álvarez <josealvar@microsoft.com>
2023-04-26 15:04:44 -0700

    Added test file

---
+++ sdk/openai/azure-ai-openai/src/test/java/com/azure/ai/openai/generated/OpenAIInferenceTests.java
@@ -0,0 +1,9 @@
+package com.azure.ai.openai.generated;
+
+import org.junit.jupiter.api.Test;
+
+public class OpenAIInferenceTests extends OpenAIClientTestBase {
+
+    @Test
+    public void Authenticate() throws Exception {}
+}
